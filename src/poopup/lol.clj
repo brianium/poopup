8287ed293d3f1c927b9e6b8bd9262cf604f52b1f
Brian Scaturro <scaturrob@gmail.com>
2025-05-14 13:55:03 -0400

    chore: process-builder always includes dev alias

--- src/poopup/lol.clj
+++ src/poopup/lol.clj
@@ -4,6 +4,6 @@
   "Display an image in a separate JVM. image-path is always stringified (handy if you want to pass something like a java.net.URL)"
   [image-path]
   (let [process-builder (ProcessBuilder.
-                         ["clojure" "-M" "-m" "poopup.main" (str image-path)])]
+                         ["clojure" "-M:dev" "-m" "poopup.main" (str image-path)])]
     (.inheritIO process-builder)
     (.start process-builder)))
